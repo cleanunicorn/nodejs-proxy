ae37a32f16ff8ec81a4e5965af7ef3a66d2c73b5
Luca Daniel-Costin <lucadanielcostin@gmail.com>
2013-01-23 00:58:20 +0200

    server starts and wait for a a connection on port 8080

--- server/server.js
+++ server/server.js
@@ -1,3 +1,18 @@
 var http = require( 'http' );
 
+http.createServer( function( request, response )
+{
+	request.on( "end", function()
+	{
+		response.writeHead( 
+			200, 
+			{
+				'Content-Type' : 'text/plain'
+			} 
+		);
+
+		response.end( 'Hello HTTP!' );
+	} );
+} ).listen( 8080 );
+
 console.log( 'all ok' );
