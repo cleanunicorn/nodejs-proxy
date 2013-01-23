325a454d6e579ee419b4e52fa4a4a2791fd2af2f
Luca Daniel-Costin <lucadanielcostin@gmail.com>
2013-01-23 02:07:42 +0200

    first version of a working proxy

--- server/server.js
+++ server/server.js
@@ -1,18 +1,58 @@
 var http = require( 'http' );
+var url = require( 'url' );
 
 http.createServer( function( request, response )
 {
-	request.on( "end", function()
-	{
-		response.writeHead( 
-			200, 
-			{
-				'Content-Type' : 'text/plain'
-			} 
-		);
+	var url_parts = url.parse( request.url );
 
-		response.end( 'Hello HTTP!' );
+	var options = {
+		hostname 	: request.headers.host,
+		port 		: 80,
+		path 		: url_parts.path,
+		method 		: request.method, 
+		headers		: request.headers
+	};
+
+	var request_data;
+
+	var proxy_client = http.request( options, function( res )
+	{
+		console.log( 'Sending request ', options );
+
+		res.on( 'data', function ( chunk )
+		{
+			console.log( 'Write to client ', chunk.length );
+			response.write( chunk, 'binary' );
+		} );
+
+		res.on( 'end', function()
+		{
+			console.log( 'End chunk write to client' );
+			response.end();
+		} );
+
+		response.writeHead( res.statusCode, res.headers );
 	} );
+
+	request.on( 'data', function ( chunk )
+	{
+		console.log( 'Write to server ', chunk.length );
+		console.log( chunk.toString( 'utf8' ) );
+		request_data = request_data + chunk;
+		proxy_client.write( chunk, 'binary' );
+	} );
+
+	request.on( 'end', function()
+	{
+		console.log( 'End chunk write to server' );
+		proxy_client.end();
+	} );
+
+	request.on( 'error', function ( e )
+	{
+		console.log( 'Problem with request ', e );
+	} );
+
 } ).listen( 8080 );
 
 console.log( 'all ok' );
